fe7edcbf7100e0d011900b9780eeb86349baf196
sfnvm <trungpt.dev@gmail.com>
2022-07-20 14:24:21 +0700

    init

---
+++ src/main/java/com/example/kafka/config/KafkaInternalConfig.java
@@ -0,0 +1,127 @@
+package com.example.kafka.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.ContainerProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaInternalConfig {
+    // Common
+    @Value("${hddt.kafka.internal.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${hddt.kafka.concurrency:100}")
+    private Integer concurrency;
+
+    // Security
+    // @Value("${hddt.kafka.security-protocol}")
+    // private String securityProtocol;
+    //
+    // @Value("${hddt.kafka.sasl.mechanism}")
+    // private String saslMechanism;
+    //
+    // @Value("${hddt.kafka.internal.sasl.jaas-config}")
+    // private String saslJAAS;
+
+    // Consumer
+    @Value("${hddt.kafka.internal.consumer.group-id:message}")
+    private String groupId;
+
+    @Value("${hddt.kafka.internal.consumer.max-poll-records:30}")
+    private Integer maxPollRecords;
+
+    @Value("${hddt.kafka.internal.consumer.max-poll-interval:600000}")
+    private Integer maxPollInterval;
+
+    @Value("${hddt.kafka.internal.consumer.session-timeout:30000}")
+    private Integer sessionTimeout;
+
+    @Value("${hddt.kafka.internal.consumer.receive-buffer:1000000}")
+    private Integer receiveBuffer;
+
+    @Value("${hddt.kafka.internal.consumer.max-fetch-bytes:2000000}")
+    private Integer maxFetchBytes;
+
+    @Value("${hddt.kafka.internal.producer.max-request-size:2000000}")
+    private Integer maxRequestSize;
+
+    private Map<String, Object> initConfig() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        // Security
+        // props.put(AdminClientConfig.SECURITY_PROTOCOL_CONFIG, securityProtocol);
+        // props.put(SaslConfigs.SASL_MECHANISM, saslMechanism);
+        // props.put(SaslConfigs.SASL_JAAS_CONFIG, saslJAAS);
+        // Max size fetch
+        props.put(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG, maxFetchBytes);
+        props.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, maxRequestSize);
+        return props;
+    }
+
+    @Bean
+    public Map<String, Object> producerInternalConfigs() {
+        Map<String, Object> props = initConfig();
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        // props.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, maxRequestSize);
+        return props;
+    }
+
+    @Bean
+    public ProducerFactory<String, String> producerInternalFactory() {
+        return new DefaultKafkaProducerFactory<>(producerInternalConfigs());
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaInternalTemplate() {
+        return new KafkaTemplate<>(producerInternalFactory());
+    }
+
+    @SuppressWarnings("Duplicates")
+    @Bean
+    public Map<String, Object> consumerInternalConfigs() {
+        Map<String, Object> props = initConfig();
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, maxPollRecords);
+        props.put(ConsumerConfig.RECEIVE_BUFFER_CONFIG, receiveBuffer);
+        props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, maxPollInterval);
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, sessionTimeout);
+        // Allows a pool of processes to divide the work of consuming and processing records
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        // Automatically reset the offset to the earliest offset
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        // Max size fetch
+        // props.put(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG, maxFetchBytes);
+        return props;
+    }
+
+    @Bean
+    public ConsumerFactory<String, String> consumerInternalFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerInternalConfigs());
+    }
+
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> kafkaInternalListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerInternalFactory());
+        factory.setConcurrency(concurrency);
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.RECORD);
+        return factory;
+    }
+}
