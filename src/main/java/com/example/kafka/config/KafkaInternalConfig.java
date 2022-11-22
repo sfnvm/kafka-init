a2ebb95d12f4667de025b3700e9bd8a28188ffdd
sfnvm <trungpt.dev@gmail.com>
2022-11-22 10:46:18 +0700

    more refactor

--- src/main/java/com/example/kafka/config/KafkaInternalConfig.java
+++ src/main/java/com/example/kafka/config/KafkaInternalConfig.java
@@ -1,7 +1,7 @@
 package com.example.kafka.config;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -20,6 +20,30 @@ import java.util.Map;
 public class KafkaInternalConfig {
   private final KafkaCommonConfig kafkaCommonConfig;
 
+  /**
+   * <h2>Consumer Config</h2>
+   */
+  @Value("${hddt.kafka.internal.consumer.group-id:message}")
+  private String groupId;
+
+  @Value("${hddt.kafka.internal.consumer.max-poll-records:30}")
+  private Integer maxPollRecords;
+
+  @Value("${hddt.kafka.internal.consumer.max-poll-interval:600000}")
+  private Integer maxPollInterval;
+
+  @Value("${hddt.kafka.internal.consumer.session-timeout:30000}")
+  private Integer sessionTimeout;
+
+  @Value("${hddt.kafka.internal.consumer.receive-buffer:1000000}")
+  private Integer receiveBuffer;
+
+  @Value("${hddt.kafka.internal.consumer.max-fetch-bytes:2000000}")
+  private Integer maxFetchBytes;
+
+  @Value("${hddt.kafka.internal.producer.max-request-size:2000000}")
+  private Integer maxRequestSize;
+
   @Value("${hddt.kafka.concurrency:10}")
   private Integer concurrency;
 
@@ -30,10 +54,7 @@ public class KafkaInternalConfig {
 
   @Bean
   public Map<String, Object> producerInternalConfigs() {
-    Map<String, Object> props = kafkaCommonConfig.initConfig();
-    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-    return props;
+    return kafkaCommonConfig.initConfig();
   }
 
   @Bean
@@ -46,9 +67,18 @@ public class KafkaInternalConfig {
     return new KafkaTemplate<>(producerInternalFactory());
   }
 
+  @SuppressWarnings("Duplicates")
   @Bean
   public ConsumerFactory<String, String> consumerInternalFactory() {
-    return new DefaultKafkaConsumerFactory<>(kafkaCommonConfig.adaptedConfigs());
+    Map<String, Object> adaptedConfigs = kafkaCommonConfig.adaptedConfigs();
+    adaptedConfigs.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, maxPollRecords);
+    adaptedConfigs.put(ConsumerConfig.RECEIVE_BUFFER_CONFIG, receiveBuffer);
+    adaptedConfigs.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, maxPollInterval);
+    adaptedConfigs.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, sessionTimeout);
+    adaptedConfigs.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+    adaptedConfigs.put(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG, maxFetchBytes);
+    adaptedConfigs.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, maxRequestSize);
+    return new DefaultKafkaConsumerFactory<>(adaptedConfigs);
   }
 
   @Bean
